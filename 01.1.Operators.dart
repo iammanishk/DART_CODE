f8d94b03f987a3c65cdccd8eaced47cd01cc4c9e
iammanishk <iammaanisshhh@gmail.com>
2023-01-19 19:35:48 +0530

    New file added and updateed

---
+++ 01.1.Operators.dart
@@ -0,0 +1,54 @@
+void main() {
+  // Add -> +;
+  // Sub -> -;
+  // Multiply -> *;
+  // Divide -> /;
+  // Reminder -> %;
+  // Proper decimal -> ~/    (This gives you the proper decimal number remove the number after the decimal);
+
+  /*var a = 5, b = 3;
+
+  var add = a + b;
+  print(add);
+  var sub = a - b;
+  print(sub);
+  var mul = a * b;
+  print(mul);
+  var div = a / b;
+  print(div);
+  var rem = a % b;
+  print(rem);
+  var pd = a ~/ b;
+  print(pd);
+*/
+
+// Incriment and Decriment..
+  /*
+ 1. ++i (incriment first then print the value)
+ 2. i++ (print the value then incrment the value)
+ 3. --i (decrment first then print the value)
+ 4. i-- (print the vaue then decrement)
+}
+*/
+
+//Logical Operator
+//logical AND
+/*
+1. true && true = true;
+2. true && false = false;
+3. false && true = false;
+4. false && false = false;
+*/
+//Logical OR;
+/*
+true || true = true;
+true || false = true;
+false || false = ture;
+false || false = false;
+*/
+  var lst;
+
+  lst = ["akash", "kumar", "manish"];
+  print(lst);
+}
+
---
+++ 01.datatype.dart
@@ -0,0 +1,17 @@
+void main() {
+  int a = 10;
+  double b = 10.5;
+  String c = "Manish";
+  bool d = true;
+  var e = "Manish Yadhuvanshi";
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(e);
+  print(a.runtimeType);
+  print(b.runtimeType);
+  print(c.runtimeType);
+  print(d.runtimeType);
+  print(e.runtimeType);
+}
